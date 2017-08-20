ad3334251e5623d99235520af07ccd8f2dcb82ac
Harish <kharishvarma@gmail.com>
2017-08-20 11:07:32 +0530

    initial commit

---
+++ deploy_source.sh
@@ -0,0 +1,8 @@
+export SITENAME=superlists.stage
+export SOURCE_REPO=https://github.com/starkhv/superlists.git
+
+mkdir -p ~/sites/$SITENAME/database
+mkdir -p ~/sites/$SITENAME/static
+mkdir -p ~/sites/$SITENAME/virtualenv
+
+git clone $SOURCE_REPO ~/sites/$SITENAME/source
