3bd267c110c10ae6f07d74c9ec63e54c2d1bcb5a
Harish <kharishvarma@gmail.com>
2017-08-20 11:11:41 +0530

    changed virtualenv to env folder

--- deploy_source.sh
+++ deploy_source.sh
@@ -3,6 +3,6 @@ export SOURCE_REPO=https://github.com/starkhv/superlists.git
 
 mkdir -p ~/sites/$SITENAME/database
 mkdir -p ~/sites/$SITENAME/static
-mkdir -p ~/sites/$SITENAME/virtualenv
+mkdir -p ~/sites/$SITENAME/env
 
 git clone $SOURCE_REPO ~/sites/$SITENAME/source
